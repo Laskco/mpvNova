7cd9eca84e6a3bc96d58991fb9918cc8518f9eb3
YLaskco <2punksdnb@gmail.com>
2026-04-21 21:03:39 -0400

    Update fork attribution

--- app/src/main/java/is/xyz/mpv/MainActivity.kt
+++ app/src/main/java/is/xyz/mpv/MainActivity.kt
@@ -11,10 +11,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
         supportActionBar?.setTitle(R.string.mpv_activity)
 
-        // The original plan was to have the file/doc picker live as fragments
-        // under here but that requires refactoring I'm really not willing to figure out now.
-        // ~sfan5, 2022-06-30
-
         if (savedInstanceState == null) {
             with (supportFragmentManager.beginTransaction()) {
                 setReorderingAllowed(true)
